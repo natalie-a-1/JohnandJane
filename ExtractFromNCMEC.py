e0b6e604c21dd1556d4baa5a2bf40e8123f47cf9
nataliehill <natalie.a.hill-1@ou.edu>
2022-07-14 17:58:52 -0500

    started class to retrieve data from NCMEC

---
+++ ExtractFromNCMEC.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+
+class RetrieveData:
+    session = requests.session()
+    firstName1 = "Jane"
+    firstName2 = "John"
+    lastName = "Doe"
+    state_abbrev = "CA"
+    response = session.get(
+        "http://www.missingkids.com/missingkids/servlet/JSONDataServlet?action=publicSearch&searchLang=en_US&search=new&subjToSearch=child&missState=" + state_abbrev + "&missCountry=US")
+    print("{} {}").format(response.status_code, response.reason)
