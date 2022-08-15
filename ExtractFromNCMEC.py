80434ee3d36fd5ac4b2d1c539928152b912d94dc
nataliehill <natalie.a.hill-1@ou.edu>
2022-08-14 21:39:41 -0500

    test

--- ExtractFromNCMEC.py
+++ ExtractFromNCMEC.py
@@ -1,13 +1,32 @@
 import json
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
 class RetrieveData:
-    session = requests.session()
-    firstName1 = "Jane"
-    firstName2 = "John"
-    lastName = "Doe"
-    state_abbrev = "CA"
-    response = session.get(
-        "http://www.missingkids.com/missingkids/servlet/JSONDataServlet?action=publicSearch&searchLang=en_US&search=new&subjToSearch=child&missState=" + state_abbrev + "&missCountry=US")
-    print("{} {}").format(response.status_code, response.reason)
+    johnRequests = requests.get(
+        'https://api.missingkids.org/missingkids/servlet/JSONDataServlet', timeout=1)
+
+    janeRequests = requests.get(
+        'https://www.missingkids.org/gethelpnow/search/poster-results')
+
+    if (johnRequests.status_code != 200 or janeRequests.status_code != 200):
+        print("Error occured while connecting to HTTP, status code is: " +
+              str(johnRequests.status_code))
+
+    johnRequests.encoding = 'utf-8'
+    # johnRequests.text
+    data = johnRequests.text
+
+    print(data)
+
+    # print(data)
+
+    #each individual person is known as a screen and has a corresponding id#
+    #posterData.caseNumber // posterData.seqNumber
+    # ng-attr-data-url
+    # data-json
+    # http://www.missingkids.org/missingkids/servlet/JSONDataServlet?action=longTermMissing&term=year&start=15&end=10&LanguageId=en_US
+    # /missingkids/servlet/PublicHomeServlet
+    # /content\/ dam\/ missingkids\/ pdfs | content\/ dam\/ netsmartz\/ pdfs | content\/ dam\/ netsmartz\/ downloadable | content\/ dam\/ kidsmartz\/ pdfs/
