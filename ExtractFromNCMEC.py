bd945c2f72051df543b6c3c832fdfe78df46e0ce
pdj555 <preston.d.jones-1@ou.edu>
2022-08-14 21:40:34 -0500

    .

--- ExtractFromNCMEC.py
+++ ExtractFromNCMEC.py
@@ -11,3 +11,4 @@ class RetrieveData:
     response = session.get(
         "http://www.missingkids.com/missingkids/servlet/JSONDataServlet?action=publicSearch&searchLang=en_US&search=new&subjToSearch=child&missState=" + state_abbrev + "&missCountry=US")
     print("{} {}").format(response.status_code, response.reason)
+    
